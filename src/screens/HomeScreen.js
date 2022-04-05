ce126df60bdf8b2d77ca0c419c7ed79533bdc46a
jacoobanderson <j00cwa@gmail.com>
2022-04-05 21:22:14 +0200

    Create design structure home

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -13,6 +13,14 @@ const HomeScreen = () => {
                 <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue.</h4>
                 <Link to='/register'>Join now</Link>
             </div>
+            <div className='secondsection'>
+                <div className='homeimage'></div>
+                <div className='hometext'><h1>Lorem ipsum dolor sit amet</h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue.</div>
+            </div>
+            <div className='thirdsection'>
+                <div className='homeimage'></div>
+                <div className='hometext'><h1>Lorem ipsum dolor sit amet</h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue.</div>
+            </div>
         </div>
     </div>
   )
