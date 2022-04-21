370e5e4e806978574c6a95fcad900a0579e2bf35
jacoobanderson <j00cwa@gmail.com>
2022-04-21 11:39:36 +0200

    Add get users fetch

--- src/pages/PrivateOverview.js
+++ src/pages/PrivateOverview.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 // import { UserContext } from '../hooks/UserContext'
 import './PrivateOverview.css'
 import PrivateNavbar from '../components/PrivateNavbar.js'
+import { useParams } from 'react-router-dom'
 // import ProfileSummary from '../components/ProfileSummary.js'
 /**
  *
  */
 const PrivateOverview = () => {
   // const user = useContext(UserContext).user
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const users = await getUsers()
+      console.log(users)
+    }
+    fetchUsers()
+  }, [])
+
+  /**
+   *
+   */
+  const getUsers = async () => {
+    const response = await fetch(
+      process.env.REACT_APP_ACCOUNT_API + `/user/${id}/all`,
+      {
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include'
+      }
+    )
+    return await response.json()
+  }
+
   return (
     <div className='overviewcontainer'>
       <PrivateNavbar />
