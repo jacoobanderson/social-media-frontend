391d2216d8420bbe3d798f8c75986e89265ad1fc
jacoobanderson <j00cwa@gmail.com>
2022-04-24 16:07:43 +0200

    Fix match functionality

--- src/pages/PrivateOverview.js
+++ src/pages/PrivateOverview.js
@@ -16,8 +16,9 @@ const PrivateOverview = () => {
   /**
    * Handles the click on connect button, gets the next user by index.
    */
-  const handleConnect = () => {
-    connect()
+  const handleConnect = async () => {
+    await connect()
+    await checkIfMatch()
     setUserIndex(userIndex + 1)
   }
 
@@ -32,9 +33,32 @@ const PrivateOverview = () => {
   /**
    *
    */
+     const checkIfMatch = async () => {
+      const response = await fetch(process.env.REACT_APP_ACCOUNT_API + `/user/${id}/friends/check`, {
+        method: 'PATCH',
+        mode: 'cors',
+        credentials: 'include',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          id: `${users[userIndex].id}`
+        })
+      })
+      
+      if (response.status === 204) {
+        console.log('MAAAAATCH')
+      }
+  
+      console.log(response)
+    }
+
+  /**
+   *
+   */
   const connect = async () => {
     const response = await fetch(process.env.REACT_APP_ACCOUNT_API + `/user/${id}/friends/accept`, {
-      method: 'PUT',
+      method: 'PATCH',
       mode: 'cors',
       credentials: 'include',
       headers: {
@@ -53,7 +77,7 @@ const PrivateOverview = () => {
    */
   const next = async () => {
     const response = await fetch(process.env.REACT_APP_ACCOUNT_API + `/user/${id}/friends/decline`, {
-      method: 'PUT',
+      method: 'PATCH',
       mode: 'cors',
       credentials: 'include',
       headers: {
