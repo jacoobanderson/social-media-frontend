cbe7ce1a19ccb7a40b463e1c5ab610372627d217
jacoobanderson <j00cwa@gmail.com>
2022-04-28 17:24:41 +0200

    Fix friend list layout

--- src/components/FriendCard.js
+++ src/components/FriendCard.js
@@ -1,9 +1,14 @@
 import './FriendCard.css'
 import React from 'react'
 
+/**
+ *
+ * @param root0
+ * @param root0.friend
+ */
 const FriendCard = ({ friend }) => {
   return (
-    <div>{friend.firstName + ' ' + friend.lastName}</div>
+    <div className='friendCard'><img src={friend.image} className='friendimg' alt='Profile Picture' />{friend.firstName + ' ' + friend.lastName}</div>
   )
 }
 
--- src/pages/Friends.js
+++ src/pages/Friends.js
@@ -3,6 +3,9 @@ import './Friends.css'
 import FriendsList from '../components/FriendsList.js'
 import PrivateNavbar from '../components/PrivateNavbar'
 
+/**
+ *
+ */
 const Friends = () => {
   return (
     <div className='friends'><PrivateNavbar/> <FriendsList /></div>
