4a7a79040619764bd77dcf2b4c159dd55dc5eb52
jacoobanderson <j00cwa@gmail.com>
2022-05-08 19:58:24 +0200

    Create base chat functionality

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,16 +1,38 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import io from 'socket.io-client'
+import './Chat.css'
+import { UserContext } from '../../hooks/UserContext'
 
+const socket = io.connect(process.env.REACT_APP_BASE_URL)
 
 const Chat = () => {
-  const socket = io.connect(process.env.REACT_APP_BASE_URL)
+  const user = useContext(UserContext).user
+    
+
+ useEffect(() => {
+     socket.on('message', ({ name, message }) => {
+         setDisplay([...display, { name, message }])
+     })
+ })
+  const [value, setValue] = useState({ name: '', message: '' })
+  const [display, setDisplay] = useState([])
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    console.log(event.target.message.value)
+    setValue({ ...value, [user.username]: event.target.message.value })
+    socket.emit('message', { name: user.username, message: event.target.message.value })  
+  }
+
   return (
     <div className='chatContainer'>
-        <form>
-            <div className='chatField'>
-            </div>
+        <form onSubmit={onSubmit}>
+            asdasda
+            {console.log(display)}
+            <div className='chatField'>asdasdas</div>
             <div className='chatInput'>
-                <input></input>
+                <input name='message'></input>
+                <input type='submit' />
             </div>
         </form>
     </div>
