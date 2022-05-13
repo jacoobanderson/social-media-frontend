db25906f75a8b04b2146fb9b13d286e32f7df1aa
jacoobanderson <j00cwa@gmail.com>
2022-05-13 19:54:34 +0200

    Fix message different sides depending on sender

--- src/components/Chat/ChatMessage.js
+++ src/components/Chat/ChatMessage.js
@@ -3,9 +3,11 @@ import './ChatMessage.css'
 
 const ChatMessage = ({ name, message, sender }) => {
   return (
-  <div className={sender ? 'currentUserMessage' : 'userMessage'}>
-    <div>{name}</div>
-    <div>{message}</div>
+  <div className={sender ? 'messageContainer' : 'messageContainerTwo'}>
+    <div className={sender ? 'currentUserMessage' : 'userMessage'}>
+        <div>{name}</div>
+        <div>{message}</div>
+    </div>
   </div>
   )
 }
