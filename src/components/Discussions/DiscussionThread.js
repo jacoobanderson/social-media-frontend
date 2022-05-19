cc819b91679b0a6ffe9e5a4897b0cb6469092675
jacoobanderson <j00cwa@gmail.com>
2022-05-19 16:54:08 +0200

    Create toggle comment functionality

--- src/components/Discussions/DiscussionThread.js
+++ src/components/Discussions/DiscussionThread.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './DiscussionThread.css'
 
 const DiscussionThread = ({ discussion }) => {
+  const [comments, setComments] = useState(false)
   return (
     <div className='discussionThreadContainer'>
             <h3>{discussion.title}: By {discussion.owner}</h3>
             <p>{discussion.content}</p>
+            <div className=''><button onClick={() => setComments(!comments)}>Comments</button>: ({discussion.replies ? discussion.replies.length : 0})</div>
+        {comments ? <div>{discussion.replies?.map((replies, index) => <div key={index}>test</div>)}<div><input type='text'></input></div></div> : null}
         <div>
 
         </div>
