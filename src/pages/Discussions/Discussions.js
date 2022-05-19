bea23d3dff3e24dd3e0684dbad36031eb81d5b16
jacoobanderson <j00cwa@gmail.com>
2022-05-19 15:40:33 +0200

    Display discussions

--- src/pages/Discussions/Discussions.js
+++ src/pages/Discussions/Discussions.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DiscussionForm from '../../components/Forms/DiscussionForm.js'
 import PrivateNavbar from '../../components/Navigation/PrivateNavbar.js'
 import './Discussions.css'
@@ -7,11 +7,34 @@ import './Discussions.css'
  *
  */
 const Discussions = () => {
+  const [discussions, setDiscussions] = useState([])
+
+  useEffect(() => {
+    getDiscussions()
+  }, [])
+  /**
+   * Gets all discussions.
+   *
+   * @param {object} event The data of the event.
+   */
+  const getDiscussions = async () => {
+    try {
+      const response = await fetch(process.env.REACT_APP_ACCOUNT_API + '/discussions/all')
+      const res = await response.json()
+      res.reverse()
+      setDiscussions(res)
+    } catch (err) {
+      console.log(err)
+    }
+  }
   return (
     <div className='discussionContainer'>
         <PrivateNavbar />
         <div className='discussionMainContent'>
             <DiscussionForm />
+            <div className='discussionFeed'>
+                {discussions.map((discussion, index) => <div key={index}>{discussion.title}</div>)}
+            </div>
         </div>
     </div>
   )
